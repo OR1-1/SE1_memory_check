159daffb1949a41d2d1692a177fa4bf1b5ef4ea9
loven <loven@localhost.localdomain>
2017-06-15 22:18:12 +0800

    get parameters

--- memory_check.sh
+++ memory_check.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-crit=10
-warn=5
+usage="USAGE:\n    ./memory_check -c [critical threshold (percentage)] -w [warning threshold (percentage)] -e [email address to send the report]"
+
+while getopts "c:w:e:" o; do
+    case "${o}" in
+        c)
+            crit=${OPTARG}
+            ;;
+        w)
+            warn=${OPTARG}
+            ;;
+        e)
+            email=${OPTARG}
+            ;;
+        \?)
+            echo -e $usage
+            exit
+            ;;
+
+    esac
+done
+
+if [ -z $crit ] || [ -z $warn ] || [ -z $email ]; then
+    echo -e $usage
+    exit
+fi
+
+if [ "$warn" -ge "$crit" ]; then
+    echo "Critical threshold should be greater than the warning threshold."
+    echo -e $usage
+    exit
+fi
 
 total_mem=$(free | grep Mem: | awk ' { print $2 } ' )
 current_mem=$(free | grep Mem: | awk ' { print $3 } ' )
@@ -10,17 +39,17 @@ used_mem_percentage=$(bc -l <<< $current_mem/$total_mem)
 used_mem_percentage=$(bc -l <<< $used_mem_percentage*100)
 used_mem_percentage=${used_mem_percentage%.*}
 
-echo 'total: '$total_mem
-echo 'current: '$current_mem
-echo 'usage: '$used_mem_percentage '%'
-echo 'warn: '$warn
-echo 'crit: '$crit
+echo 'Total Memory: '$total_mem
+echo 'Current Memory: '$current_mem
+echo 'Usage: ' $used_mem_percentage '%'
+echo 'Warning Threshold: '$warn
+echo 'Critical Threshold: '$crit
 
 if [ "$used_mem_percentage" -ge "$crit" ]; then
-    echo 'CRIT!!'
+    echo 'CRITICAL!!'
     exit 2 
 elif [ "$used_mem_percentage" -ge "$warn" ] && [ "$used_mem_percentage" -lt "$crit" ]; then
-    echo 'WARN!!'
+    echo 'WARNING!!'
     exit 1
 else     
     echo 'OK!!'
